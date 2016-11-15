15c0bf3eece6b371f3c9a0679c85d5797417c663
Vincent Gentile <DragonSlayer@dhcp-10-8-000-171.mobile.reshsg.uci.edu>
2016-11-15 13:13:28 -0800

    Initial Commit

---
+++ BeyondTinnitus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeyondTinnitus
+//
+//  Created by Vincent Gentile on 11/15/16.
+//  Copyright © 2016 Vincent Gentile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
