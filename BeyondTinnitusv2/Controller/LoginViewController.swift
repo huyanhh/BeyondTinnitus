e2d9a2ae8155a9ef2aa38f732d33bfee3b1bbb7f
Huyanh Hoang <huyanhh@uci.edu>
2016-11-22 14:13:15 -0800

    keyboard notifications

--- BeyondTinnitusv2/Controller/LoginViewController.swift
+++ BeyondTinnitusv2/Controller/LoginViewController.swift
@@ -13,6 +13,9 @@ class LoginViewController: UIViewController {
     
     @IBOutlet weak var pwd: UITextField!
     @IBOutlet weak var uid: UITextField!
+    fileprivate var keyboardSize: CGRect?
+    fileprivate var backgroundTapGesture: UITapGestureRecognizer?
+
     
     @IBAction func signIn() {
         guard let controller = self.storyboard?.instantiateViewController(withIdentifier: "main") as? UINavigationController
@@ -26,6 +29,49 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        registerForKeyboardNotifications()
+    }
+    
+    func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWasShown), name: NSNotification.Name.UIKeyboardDidShow, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillBeHidden), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    func keyboardWasShown(aNotification: NSNotification) {
+        if let userInfo: NSDictionary = aNotification.userInfo! as NSDictionary? {
+            keyboardSize = (userInfo[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue
+        }
+        
+        UIView.animate(withDuration: 0.1, delay: 0, options: .curveEaseOut, animations: {
+            self.view.frame.origin = CGPoint(x: 0, y: -self.keyboardSize!.height)
+        }, completion: nil)
+    }
+    
+    func keyboardWillBeHidden(aNotification: NSNotification) {
+        UIView.animate(withDuration: 0.1) {
+            self.view.frame.origin = CGPoint(x: 0, y: 0)
+        }
     }
 }
+
+extension LoginViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        view.resignFirstResponder()
+        return true
+    }
+
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        backgroundTapGesture = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        view.addGestureRecognizer(backgroundTapGesture!)
+        hideKeyboardWhenTappedAround()
+    }
+    
+    override func dismissKeyboard() {
+        view.endEditing(true)
+        view.removeGestureRecognizer(backgroundTapGesture!)
+    }
+
+}
+
---
+++ BeyondTinnitusv2/Utility/Utility.swift
@@ -0,0 +1,21 @@
+//
+//  Utility.swift
+//  BeyondTinnitusv2
+//
+//  Created by Huyanh Hoang on 2016. 11. 22..
+//  Copyright © 2016년 teamMedApp. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let backgroundTapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        view.addGestureRecognizer(backgroundTapGesture)
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
