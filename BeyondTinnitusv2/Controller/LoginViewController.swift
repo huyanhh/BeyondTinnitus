9d15d8b2039b5af7678d852dd9799324ef89e967
Huyanh Hoang <huyanhh@uci.edu>
2017-02-09 20:39:27 -0800

    firebase download works

--- BeyondTinnitusv2/Controller/LoginViewController.swift
+++ BeyondTinnitusv2/Controller/LoginViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import AVFoundation
 
 class LoginViewController: UIViewController {
     
@@ -15,7 +16,9 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var uid: UITextField!
     fileprivate var keyboardSize: CGRect?
     fileprivate var backgroundTapGesture: UITapGestureRecognizer?
-
+    var tone: AVAudioPlayer!
+    
+    
     
     @IBAction func signIn() {
         guard let controller = self.storyboard?.instantiateViewController(withIdentifier: "main") as? UINavigationController
@@ -27,9 +30,88 @@ class LoginViewController: UIViewController {
         }
     }
     
+    @IBAction func playAudioFile() {
+//        let path = Bundle.main.path(forResource: "Tf-8000.mp4", ofType: nil)!
+//        let url = URL(fileURLWithPath: path)
+        fetchFile()
+    }
+    
+    @IBAction func playforreal() {
+        let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        let documentsDirectory = paths[0]
+        let filePath = "\(documentsDirectory)/soundpoo.mp4"
+        print("file path")
+        let url = URL(fileURLWithPath: filePath)
+        print(url)
+        do {
+            let sound = try AVAudioPlayer(contentsOf: url)
+            
+            tone = sound
+            sound.play()
+        } catch {
+            // couldn't load file
+            print("couldnt load file")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         registerForKeyboardNotifications()
+    }
+    
+    func playAudioWith(file: URL) {
+        do {
+            let sound = try AVAudioPlayer(contentsOf: file)
+            tone = sound
+            tone.play()
+        } catch {
+            // couldn't load file
+            print("couldnt load file")
+        }
+    }
+    
+    func fetchFile() {
+        
+        let storage = FIRStorage.storage()
+        
+        // Create a storage reference from our storage service
+        let storageRef = storage.reference(forURL: "gs://beyondtinnitus-340f1.appspot.com")
+        let soundRef = storageRef.child("soundtest.mp4")
+        
+        // Create local filesystem URL
+//        let localURL = URL(string: "path/to/image")!
+        let localURL = getDocsURL()
+        
+        // Download to the local filesystem
+        soundRef.write(toFile: localURL) { url, error in
+            if let error = error {
+                // Uh-oh, an error occurred!
+                print("some error")
+                print(error)
+            } else {
+                // Local file URL for "images/island.jpg" is returned
+                print("success")
+                print(url!)
+                self.playAudioWith(file: url!)
+            }
+        }
+
+    }
+    
+    func getDocsURL() -> URL {
+        let filePath = "file:/sound.mp4"
+        guard var docsurl = URL.init(string: filePath) else { fatalError() }
+        do {
+            let fm = FileManager.default
+            docsurl = try fm.url(for: .documentDirectory, in: .userDomainMask,
+                                     appropriateFor: nil, create: false)
+            docsurl.appendPathComponent("soundpoo.mp4")
+            
+        } catch {
+            // deal with error here
+            print("error")
+        }
+        return docsurl
     }
     
     func registerForKeyboardNotifications() {
